e1409007a7f387a0cb14edce14e319d9f550b6fe
Karam Ajaj <karam.ajaj@hotmail.com>
2023-06-05 15:39:16 +0200

    Initial commit

---
+++ Constants/RequestConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace RESTfulAPIConsume.Constants
+{
+    
+    public static class RequestConstants {
+
+
+        public const string APIUrl = "https://VeeamOne.FQDN:1239/api/";
+        public const string get_token = APIUrl + "token";
+        public const string format_json = "format=json";
+        public const string format_xml = "format=xml";
+        
+        public const string path_json = @"D:\folder\downloads\report.json";
+        public const string path_csv = @"D:\folder\downloads\report.csv";
+        public const string path_xlsx = @"D:\folder\downloads\report.xlsx";
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# VeeamOne-reports
+ C# app to get reports from VeeamOne API and deliver it to a location on a file server
